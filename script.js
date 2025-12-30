4764425b4c8ba48e303e2f8bb8f77babf2ee36f2
Vice4347 <vice.andrescarrasco@gmail.com>
2025-12-30 00:32:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const targetDate = new Date("January 1, 2026 00:00:00").getTime();
+const targetDate = new Date("January 1, 2022 00:00:00").getTime();
 
 const countdownEl = document.getElementById("countdown");
 const lock = document.getElementById("lock");
