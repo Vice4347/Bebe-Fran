eeef589eff9daa291b5dab015ded2cf6b5c3b9d1
Vice4347 <vice.andrescarrasco@gmail.com>
2025-12-31 13:48:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -53,3 +53,16 @@ window.addEventListener("scroll", () => {
       `translateY(${window.scrollY * 0.02 * dir}px)`;
   });
 });
+
+const music = document.getElementById("music");
+const playBtn = document.getElementById("playBtn");
+
+playBtn.addEventListener("click", () => {
+  if (music.paused) {
+    music.play();
+    playBtn.textContent = "⏸ Pausar música";
+  } else {
+    music.pause();
+    playBtn.textContent = "▶ Reproducir música";
+  }
+});
